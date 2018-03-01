eb83405e6f691309fea2102c3e3e71c5e344c870
Hao Wen <wenhao@126.com>
2018-03-01 20:07:45 +0800

    [Wen Hao] - update Dockerfile.

--- xxl-job-git-executor/src/main/java/com/xxl/job/executor/service/GitService.java
+++ xxl-job-git-executor/src/main/java/com/xxl/job/executor/service/GitService.java
@@ -1,13 +1,34 @@
 package com.xxl.job.executor.service;
 
+import com.xxl.job.core.log.XxlJobLogger;
 import com.xxl.job.executor.domain.Git;
+import org.apache.commons.exec.*;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @Service
 public class GitService {
 
     public String getLatestHash(Git git) {
-        return null;
-    }
+        String command = "git ls-remote ";
 
+        CommandLine commandLine = CommandLine.parse(command);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PumpStreamHandler pumpStreamHandler = new PumpStreamHandler(outputStream);
+        ExecuteWatchdog watchdog = new ExecuteWatchdog(60000);
+        Executor executor = new DefaultExecutor();
+        executor.setExitValue(1);
+        executor.setWatchdog(watchdog);
+        executor.setStreamHandler(pumpStreamHandler);
+        try {
+            executor.execute(commandLine);
+        } catch (IOException e) {
+            XxlJobLogger.log("[ERROR] {0}, could not get latest commit hash, git server isn't available.", git.getUrlBranch());
+            XxlJobLogger.log(e);
+            return "";
+        }
+        return outputStream.toString();
+    }
 }
