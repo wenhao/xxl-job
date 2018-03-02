5b02dd330d9ae946e27c3fe96edaf74c7e40e015
Hao Wen <wenhao@126.com>
2018-03-02 10:13:25 +0800

    [Wen Hao] - gitService get latest commit hash.

--- xxl-job-git-executor/src/test/java/com/xxl/job/executor/service/GitServiceTest.java
+++ xxl-job-git-executor/src/test/java/com/xxl/job/executor/service/GitServiceTest.java
@@ -2,6 +2,7 @@ package com.xxl.job.executor.service;
 
 import com.xxl.job.executor.domain.Git;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,9 +17,10 @@ public class GitServiceTest {
     }
 
     @Test
+    @Ignore
     public void should_get_latest_git_repository_hash() {
         // given
-        Git git = new Git("https://github.com/wenhao/xxl-job.git,master,wenhao@126.com,1qaz!QAZ,pl000001");
+        Git git = new Git("https://github.com/wenhao/xxl-job.git,master,test,testpwd,pl000001");
 
         // when
         String latestHash = gitService.getLatestHash(git);
