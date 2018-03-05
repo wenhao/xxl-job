b158c2bef4f82f28fe076493749360cb8a9ac934
Hao Wen <wenhao@126.com>
2018-03-05 13:12:38 +0800

    [Wen Hao] - change spring context config.

--- xxl-job-git-executor/src/main/java/com/xxl/job/executor/repository/GitCommitRepository.java
+++ xxl-job-git-executor/src/main/java/com/xxl/job/executor/repository/GitCommitRepository.java
@@ -3,10 +3,6 @@ package com.xxl.job.executor.repository;
 import com.xxl.job.executor.entity.GitCommit;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface GitCommitRepository extends CrudRepository<GitCommit, String> {
-
-    Optional<GitCommit> findByUrlBranch(String urlBranch);
 
 }
